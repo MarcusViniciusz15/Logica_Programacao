feeedac6907c4cbd16e4c2bd3b867e7af840f170
MarcusViniciusz15 <conta6188@gmail.com>
2025-09-18 10:49:53 -0300

    .
    git commit

---
+++ Módulo 2/Aula 1/Variáveis/dom/script.js
@@ -0,0 +1,31 @@
+// document.body.style.backgroundColor = "black"
+// document.body.style.color = "white"
+
+// window.alert("Seja Bem-Vindo")
+
+// window.prompt("Login")
+// window.prompt("Senha")
+
+// window.confirm("Site com Cavalo de Tróia")
+
+
+let paragrafo1 = document.getElementsByTagName("p")[0]
+paragrafo1.innerHTML = "USANDO O TAG NAME"
+paragrafo1.style.backgroundColor = "red"
+
+
+let paragrafo2 = document.getElementById("paragrafo2")
+paragrafo2.innerHTML = "USANDO O ID"
+paragrafo2.style.backgroundColor = "green"
+
+let paragrafo3 = document.getElementsByClassName("paragrafo3")[0]
+paragrafo3.innerHTML = "USANDO A CLASSE"    
+paragrafo3.style.backgroundColor = "Blue"
+
+let paragrafo4 = document.querySelector("#paragrafo4")
+paragrafo4.innerHTML = "USANDO O QUERY SELECTOR"    
+paragrafo4.style.backgroundColor = "pink"
+
+let paragrafo5 = document.querySelectorAll("#p")[0]
+paragrafo5.innerHTML = "USANDO O QUERY SELECTOR All"    
+paragrafo5.style.backgroundColor = "purple"   
---
+++ Módulo 2/Aula 1/Variáveis/script.js
@@ -0,0 +1,1 @@
+console.log("Salve JS 😎");
