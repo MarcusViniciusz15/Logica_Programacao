a6cf2e1efa7a6a0448e0a8298b329f2c1e12b96d
MarcusViniciusz15 <conta6188@gmail.com>
2025-09-19 10:32:13 -0300

    .
    git commit

---
+++ Módulo 2/Aula 12/desafio/script2.js
@@ -0,0 +1,6 @@
+function esconder() {
+    document.getElementById("texto").style.display = "none"
+}
+function mostrar() {
+    document.getElementById("texto").style.display = "block"
+}
---
+++ Módulo 2/Aula 12/eventos DOM/script.js
@@ -0,0 +1,19 @@
+let caixa = document.getElementById("mouse")
+
+
+
+function entradaMouse() {
+    caixa.style.backgroundColor = "blue"
+}
+
+function saidaMouse() {
+    caixa.style.backgroundColor = "red"
+}
+
+function mudaHtml() {
+    let nome = document.querySelector("#nome").value
+    caixa.innerHTML = nome
+
+}
+
+
